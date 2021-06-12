01ecdd6dad94f83cd05bd0bfe6449bf618d0d9f9
binh.buidang <dangbinh4869@gmail.com>
2021-06-12 21:51:58 +0700

    add: migrations

--- API/Program.cs
+++ API/Program.cs
@@ -1,13 +1,32 @@
+using System;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Persistence;
 
 namespace API
 {
-    public static class Program
+    public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+
+            try
+            {
+                var context = services.GetRequiredService<DataContext>();
+                context.Database.Migrate();
+            }
+            catch (Exception e)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(e, "An error occured during migration");
+            }
+            host.Run();
         }
 
         private static IHostBuilder CreateHostBuilder(string[] args) =>
