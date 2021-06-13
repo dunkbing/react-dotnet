d4b528ac3ade25bf091dd51226e53bb6bb6c1c6b
binh.buidang <dangbinh4869@gmail.com>
2021-06-13 15:39:54 +0700

    add: activity list

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import {Grid, List} from "semantic-ui-react";
+import {Grid} from "semantic-ui-react";
 import {Activity} from "../../../app/models/activity";
+import ActivityList from "./ActivityList";
 
 interface Props {
   activities: Activity[];
@@ -10,13 +11,7 @@ const ActivityDashboard: React.FC<Props> = ({ activities }) => {
   return (
     <Grid>
       <Grid.Column width='10'>
-        <List>
-          {activities.map((activity: Activity) => (
-            <List.Item key={activity.id}>
-              {activity.title}
-            </List.Item>
-          ))}
-        </List>
+        <ActivityList activities={activities} />
       </Grid.Column>
     </Grid>
   );
