e353c4c6737a4f24f57749ca69630db0c19b852b
binh.buidang <dangbinh4869@gmail.com>
2021-06-13 08:59:36 +0700

    add: delete activity route

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -33,5 +33,11 @@ namespace API.Controllers
             activity.Id = id;
             return Ok(await Mediator.Send(new Edit.Command {Activity = activity}));
         }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<IActionResult> DeleteActivity(Guid id)
+        {
+            return Ok(await Mediator.Send(new Delete.Command {Id = id}));
+        }
     }
 }
