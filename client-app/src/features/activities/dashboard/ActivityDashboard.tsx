1f195369d1ce6ab00868794aed5e8185cb9db539
binh.buidang <dangbinh4869@gmail.com>
2021-06-14 09:04:11 +0700

    add: activity details and form

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import {Grid} from "semantic-ui-react";
 import {Activity} from "../../../app/models/activity";
 import ActivityList from "./ActivityList";
+import ActivityDetails from "../details/ActivityDetails";
+import ActivityForm from "../form/ActivityForm";
 
 interface Props {
   activities: Activity[];
@@ -12,6 +14,10 @@ const ActivityDashboard: React.FC<Props> = ({ activities }) => {
     <Grid>
       <Grid.Column width='10'>
         <ActivityList activities={activities} />
+      </Grid.Column>
+      <Grid.Column width='6'>
+        {activities[0] && <ActivityDetails activity={activities[0]}/>}
+        <ActivityForm />
       </Grid.Column>
     </Grid>
   );
